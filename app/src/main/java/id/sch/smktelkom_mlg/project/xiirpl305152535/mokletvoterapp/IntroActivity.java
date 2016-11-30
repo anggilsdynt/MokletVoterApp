82503bd66b4941f7295ccb58f9307d100eb8687d
fadilghulam <razerstarsdota@gmail.com>
2016-11-30 14:41:38 +0700

    App Introl #1

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl305152535/mokletvoterapp/IntroActivity.java
@@ -0,0 +1,37 @@
+package id.sch.smktelkom_mlg.project.xiirpl305152535.mokletvoterapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.github.paolorotolo.appintro.AppIntro;
+
+/**
+ * Created by microsoft on 30/11/2016.
+ */
+public class IntroActivity extends AppIntro {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addSlide(SampleSlide.newInstance(R.layout.slide_1));
+        addSlide(SampleSlide.newInstance(R.layout.slide_2));
+        addSlide(SampleSlide.newInstance(R.layout.slide_3));
+        //addSlide(AppIntroFragment.newInstance("Slide Title", "Testtttt", R.mipmap.ic_launcher, g));
+
+        setZoomAnimation();
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        // Do something when users tap on Skip button.
+        finish();
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        // Do something when users tap on Done button.
+        finish();
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl305152535/mokletvoterapp/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl305152535/mokletvoterapp/MainActivity.java
@@ -1,7 +1,8 @@
 package id.sch.smktelkom_mlg.project.xiirpl305152535.mokletvoterapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, IntroActivity.class);
+        startActivity(intent);
+
     }
+
 }
