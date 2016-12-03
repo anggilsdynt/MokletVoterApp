849a3e19f07b6befcac7007c466750eeecb339f3
Sherlock Holmes <vegaszvarri@gmail.com>
2016-12-03 19:21:32 +0700

    End Vote

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl305152535/mokletvoterapp/DaftarKandidat.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl305152535.mokletvoterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DaftarKandidat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar_kandidat);
+    }
+}
